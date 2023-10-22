c7c71a20046ef8883c553847025e3aad7eab17ca
MdAbuBokor <abfrombsmrstu@gmail.com>
2023-10-23 01:10:39 +0600

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,10 @@
 import bcryptjs from "bcryptjs";
-import User from "../models/user.model.js";
+import dotenv from "dotenv";
+dotenv.config();
 
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+import { errorHandler } from "./../utils/error.js";
 export const signup = async (req, res, next) => {
   // console.log(req.body);
   //res.send("SignUp");
@@ -14,6 +18,26 @@ export const signup = async (req, res, next) => {
     await newUser.save();
 
     res.status(201).json("new user saved");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "USer not found"));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong Credintials!"));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token ", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
